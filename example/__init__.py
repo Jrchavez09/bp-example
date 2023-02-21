4b22313616442991d9f7675aadfe9d2b7ad78dda
Jc <jrchavez@my.waketech.edu>
2023-02-20 21:48:11 -0500

    modified:   example/__init__.py

--- example/__init__.py
+++ example/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+# from flask.ext.sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -8,6 +9,8 @@ app = Flask(__name__)
 DB_NAME = 'example-dev.db'
 
 app.config['SECRET_KEY'] = 'ebf217de553d09b6c3187a6b74e2a488'
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://username:password@localhost/{DB_NAME}'
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://username:password@localhost/{DB_NAME}'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -32,11 +35,14 @@ app.register_blueprint(user_bp)
 from example.user.models import User
 
 login_manager.login_view = 'user.login'
+
+
 # login_manager.login_message_category = 'info'
 
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
 """
     DB creation
@@ -47,6 +53,7 @@ with app.app_context():
 """
     error handlers
 """
+
 
 @app.errorhandler(403)
 def forbidden_page(error):
